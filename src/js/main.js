708e4b7f66402ddcffac89259324582613b72c61
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-09-25 11:55:37 +0200

    feat: list of repos

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,35 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
+  
 // place your code below
+const list = document.querySelector('.repository__list--js');
+
+fetch('https://api.github.com/users/atanvarde/repos')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for ( const repo of repos) {
+      list.innerHTML += `<li>${repo.name} ${repo.html_url}</li>`;
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 
-console.log(`Hello world!`);
+
+
 
 
